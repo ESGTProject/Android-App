6bd1644951cedc20ffa25ea23d96cdcb445a8a92
Kairi Kozuma <kairikozuma@gmail.com>
2017-03-15 01:51:55 -0400

    Load summary for preferences

--- app/src/main/java/io/github/esgtproject/esgtapp/SettingsActivity.java
+++ app/src/main/java/io/github/esgtproject/esgtapp/SettingsActivity.java
@@ -177,8 +177,10 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-//            bindPreferenceSummaryToValue(findPreference("example_text"));
-//            bindPreferenceSummaryToValue(findPreference("example_list"));
+            bindPreferenceSummaryToValue(findPreference("pref_display_name"));
+            bindPreferenceSummaryToValue(findPreference("pref_time_zone"));
+            bindPreferenceSummaryToValue(findPreference("pref_weather_location"));
+            bindPreferenceSummaryToValue(findPreference("pref_news_source"));
         }
 
         @Override
