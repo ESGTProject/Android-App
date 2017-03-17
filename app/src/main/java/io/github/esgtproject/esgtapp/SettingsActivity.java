4e18ba801f40b220afb6bb41507386b41fe6e34f
Kairi Kozuma <kairikozuma@gmail.com>
2017-03-17 11:23:54 -0400

    Bind username to summary in preferences

--- app/src/main/java/io/github/esgtproject/esgtapp/SettingsActivity.java
+++ app/src/main/java/io/github/esgtproject/esgtapp/SettingsActivity.java
@@ -139,6 +139,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_username_key)));
             bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_display_name_key)));
             bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_time_zone_key)));
             bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_weather_location_key)));
